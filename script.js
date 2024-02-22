9a9d6cab2f5cefaf7eeaba27a71b2d0f1227e6aa
Jose Aguilar <149532459+jaguilar35178@users.noreply.github.com>
2024-02-22 15:04:43 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+function game(){
+   //
+if (confirm("I'm my dad's favorite child")){
+alert("nope!");
+   } else{
+    alert("yuhhhh!");
+
+   }
+   if (confirm("I am a skilled lanscaper")){
+    alert("yuhhhh!");
+    } else{
+ alert("nope!");
+        
+    }
+    if (confirm("been driving since 12")){
+    alert("yuuhhhh!");
+    } else{
+  alert("nahhhh!");
+       
+    }
+
+    //5 questions
+    if(prompt("Is Timetravel possible?") ==="yes".toLowerCase || "y".toLowerCase){
+    console.log("question one response is yes");
+    }
+    if(prompt("Do you belive in god?") ==="yes".toLowerCase || "y".toLowerCase){
+      console.log("question one response is yes");
+    }
+    if(prompt("Do you belive in ghost?") ==="yes".toLowerCase || "y".toLowerCase){
+         console.log("question one response is yes");
+  }
+  if(prompt("Do you belive in witchcraft?") ==="yes".toLowerCase || "y".toLowerCase){
+      console.log("question one response is yes");
+      }
+      if(prompt("Can steriods decrease your year to live.") ==="yes".toLowerCase || "y".toLowerCase){
+    console.log("question one response is yes");
+   }
+
+}
+
+function getToKnowYou(){
+    let username =prompt("what is your name");
+    alert("Helloo"+ username + " This is a private website please log out!");
+   alert(username +" ,I hope you get hacked");
+
+}
